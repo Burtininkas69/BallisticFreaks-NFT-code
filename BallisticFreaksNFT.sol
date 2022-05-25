81ad58627d50155923518bee175b12cd01c021df
Gustas K <74874838+Burtininkas69@users.noreply.github.com>
2022-05-25 06:53:37 +0300

    Update BallisticFreaksNFT.sol

--- BallisticFreaksNFT.sol
+++ BallisticFreaksNFT.sol
@@ -18,6 +18,9 @@ pragma solidity >= 0.7 .0 < 0.9 .0;
 //██║░░░░░██║░░██║███████╗██║░░██║██║░╚██╗██████╔╝
 //╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract BallisticFreaksNFTs is ERC721Enumerable,
 Ownable {
     using Strings
